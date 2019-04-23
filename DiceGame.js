6c5cc5ccf31ccb2564cfb13f78c670372846046b
henry huynh <hlhuynh14@gmail.com>
2019-04-23 16:22:53 -0500

    finish skeleton of the game, need css and html plus put some varialble into functions

--- DiceGame.js
+++ DiceGame.js
@@ -23,12 +23,15 @@ let enemyrange = 350 + (5 * daysroll);
 let enemystrengthdefense = 100 + (5 * daysroll);
 let enemymagicdefense = 70 + (5 * daysroll);
 let enemyrangedefense = 100 + (5 * daysroll);
-let enemymeleeunits = diceroll(1000) + 1000;
-let enemymagicunits = diceroll(1000) + 1000;
-let enemyrangeunits = diceroll(1000) + 1000;
+let enemymeleeunits = diceroll(1000) + 500;
+let enemymagicunits = diceroll(1000) + 500;
+let enemyrangeunits = diceroll(1000) + 500;
 let meleeunits = diceroll(500) + 500;
 let magicunits = diceroll(500) + 500;
 let rangeunits = diceroll(500) + 500;
+
+let armyvalue = meleeunits + magicunits + rangeunits
+let enemyarmyvalue = enemymeleeunits + enemymagicunits + enemyrangeunits
 
 
 function start() {
@@ -62,62 +65,80 @@ increasing(training);
 }
 }
 
-function Knights() {
+function knights() {
 	let enemyknights = diceroll(3)
 	if (enemyknights == 1) {
 		armyvalue -= ((enemystrength * 2) - strengthdefensevalue)
 		enemyarmyvalue -= ((strengthvalue * 2) - enemystrengthdefense)
+		console.log("Your opponent chose knights, both sides are even, you suffered a "+ ((enemystrength * 2) - strengthdefensevalue) + " lost of troops.")
+		console.log("Your opponent lost " + ((strengthvalue * 2) - enemystrengthdefense) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}	else if (enemyknights == 2) {
 		armyvalue -= ((enemymagic * 3) - strengthdefensevalue)
 		enemyarmyvalue -= ((strengthvalue * 2) - (enemymagicdefense * 2))
+		console.log("Your opponent chose mages, their mages roasted your knights, you suffered a "+ ((enemymagic * 3) - strengthdefensevalue) + " lost of troops.")
+		console.log("Your opponent lost " + ((strengthvalue * 2) - (enemymagicdefense * 2)) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}	else  {
 		armyvalue -= ((enemyrange * 2) - strengthdefensevalue)
 		enemyarmyvalue -= ((strengthvalue * 3) - enemyrangedefense)
+		console.log("Your opponent chose archers, your knights cut through them like butter, you suffered a "+ ((enemyrange * 2) - strengthdefensevalue) + " lost of troops.")
+		console.log("Your opponent lost " + ((strengthvalue * 3) - enemyrangedefense) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}
 }
 
 
-function Mages() {
+function mages() {
 	let enemymages = diceroll(3)
 	if (enemymages == 1) {
-		armyvalue -= ((enemyknights * 2)  - (magicdefensevalue * 2))
+		armyvalue -= ((enemystrength * 2)  - (magicdefensevalue * 2))
 		enemyarmyvalue -= ((magicvalue * 3) - enemystrengthdefense)
+		console.log("Your opponent chose knights, your mages burnt them to the ground, you suffered a "+ ((enemystrength * 2)  - (magicdefensevalue * 2)) + " lost of troops.")
+		console.log("Your opponent lost " + (((magicvalue * 3) - enemystrengthdefense)) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}	else if (enemymages == 2) {
-		armyvalue -= ((enemyknights * 2) - (magicdefensevalue * 2))
+		armyvalue -= ((enemystrength * 2) - (magicdefensevalue * 2))
 		enemyarmyvalue -= ((strengthvalue * 2) - (enemymagicdefense * 2))
+		console.log("Your opponent chose mages, you are both even, you suffered a "+ ((enemystrength * 2) - (magicdefensevalue * 2)) + " lost of troops.")
+		console.log("Your opponent lost " + ((strengthvalue * 2) - (enemymagicdefense * 2)) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}	else  {
 		armyvalue -= ((enemyrange * 3) - magicdefensevalue)
 		enemyarmyvalue -= ((magicvalue * 2) - (enemyrangedefense * 2))
+		console.log("Your opponent chose archers, your opponents shot your mages down before they can fire their spells, you suffered a "+ ((enemyrange * 3) - magicdefensevalue) + " lost of troops.")
+		console.log("Your opponent lost " + ((magicvalue * 2) - (enemyrangedefense * 2)) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}
 }	
 
-function Archers() {
+function archers() {
 	let enemyarchers = diceroll(3)
 	if (enemyarchers == 1) {
-		armyvalue -= ((enemyknights * 3)  - rangedefensevalue )
+		armyvalue -= ((enemystrength * 3)  - rangedefensevalue )
 		enemyarmyvalue -= ((rangevalue * 2) - (enemystrengthdefense * 2))
+		console.log("Your opponent chose knights, your archers are masscared, you suffered a "+ (((enemystrength * 3)  - rangedefensevalue )) + " lost of troops.")
+		console.log("Your opponent lost " + ((rangevalue * 2) - (enemystrengthdefense * 2)) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}	else if (enemyarchers == 2) {
 		armyvalue -= ((enemymagic * 2) - (rangedefensevalue * 2))
 		enemyarmyvalue -= ((rangevalue * 3) - enemymagicdefense)
+			console.log("Your opponent chose mages, they are helpless before your rains of arrows, you suffered a "+ ((enemymagic * 2) - (rangedefensevalue * 2)) + " lost of troops.")
+		console.log("Your opponent lost " + ((rangevalue * 3) - enemymagicdefense) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}	else  {
 		armyvalue -= ((enemyrange * 2) - rangedefensevalue)
 		enemyarmyvalue -= ((rangevalue * 2) - enemyrangedefense)
+		console.log("Your opponent chose archers, your opponents shot your mages down before they can fire their spells, you suffered a "+ ((enemyrange * 3) - magicdefensevalue) + " lost of troops.")
+		console.log("Your opponent lost " + ((magicvalue * 2) - (enemyrangedefense * 2)) + " troops.")
 		console.log("You have " + armyvalue + " troops.")
 		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}
@@ -125,48 +146,49 @@ function Archers() {
 
 
 function turn() {
-let whichunit = prompt("Choose either to train your Knights, Mages, Archers, or Stop.")
-if (whichunit == "Knights"){ 
+let whichunit = prompt("Choose either knights, mages, archers, or stop.")
+if (whichunit == "knights"){ 
 knights();
-}	else if ( whichunit =="Mages") {
+}	else if ( whichunit =="mages") {
 	mages();
-}	else if (whichunit =="Archers") {
+}	else if (whichunit =="archers") {
 	archers();
-}	else if (whichunit == "Stop"){
-		
+}	else if (whichunit == "stop"){
+	return stop;	
 }	else {
 	alert("Please enter knights, mages, archers, or stop!");
 	turn();
 }
 }
 
+
 function increasestrength() {
-	strengthvalue += (diceroll(50) + 10);
+	strengthvalue += (diceroll(100) + 10);
 	console.log( "You have increase the strength of your Knights to" + " " + strengthvalue);
 }
 
 function increasemeleedefense() {
-	strengthdefensevalue += (diceroll(50) + 10);
+	strengthdefensevalue += (diceroll(100) + 10);
 	console.log( "You have increase the defense of your Knights to" + " " + strengthdefensevalue);
 }
 
 function increasemagic() {
-	magicvalue += (diceroll(50) + 10);
+	magicvalue += (diceroll(100) + 10);
 	console.log( "You have increase the magic of your Mages to" + " " + magicvalue);
 }
 
 function increasemagicdefense() {
-	magicdefensevalue += (diceroll(25) + 10);
+	magicdefensevalue += (diceroll(50) + 10);
 	console.log( "You have increase the defense of your Mages to" + " " + magicdefensevalue);
 }
 
 function increaserange() {
-	rangevalue += (diceroll(25) + 10);
+	rangevalue += (diceroll(50) + 10);
 	console.log( "You have increase the range of your Archers to" + " " + rangevalue);
 }
 
 function increaserangedefense() {
-	rangedefensevalue += (diceroll(25) + 10);
+	rangedefensevalue += (diceroll(50) + 10);
 	console.log( "You have increase the defense of your Archers to" + " " + rangedefensevalue);
 }
 
@@ -309,7 +331,7 @@ function starttraining(){
 				randomfactor = diceroll(100);
 			}
 			else if (randomfactor <= 20 && randomfactor > 10){
-				alert("Several adventurers has taken an arrow to the knee during practice and will have to sit out this battle!" + "You lost " + randomrange2 + " archers!")
+				alert("Several archers has taken an arrow to the knee during practice and will have to sit out this battle!" + "You lost " + randomrange2 + " archers!")
 				rangeunits -= randomrange2
 				console.log("Your currently have "+ rangeunits + " archers")
 				randomfactor = diceroll(100);
@@ -347,9 +369,6 @@ console.log(enemyrangeunits + " archers will be in the the enemy army.")
 
 alert("The battle begins!")
 
-let armyvalue = meleeunits + magicunits + rangeunits
-let enemyarmyvalue = enemymeleeunits + enemymagicunits + enemyrangeunits
-
 console.log("You have " + armyvalue + " troops.")
 console.log("The enemy have " + enemyarmyvalue + " troops.")
 
@@ -359,4 +378,17 @@ alert( "knights > archers, archers > mages, mages > knights.")
 
 while (armyvalue >= 0 && enemyarmyvalue >= 0) {
 	turn()
+
 }
+
+if (armyvalue <= 0 && enemyarmyvalue <= 0) {
+	alert("Both armies mutually destroyed eachother, but at least you did not lose your city!")
+}	else if ( armyvalue <= 0) {
+	alert ("You lost, your city will be pillaged and it's denizen will be forced to serve another city. Better luck next time!")
+}	else if (enemyarmyvalue <= 0) {
+		alert("Good job! You vanquish the enemy, take no prisoner, let them know not to mess with you.")
+}	else {
+
+}
+
+
--- fiddling.js
+++ fiddling.js
@@ -1,101 +1,224 @@
-function reversestr(str1) {
-	newstr =""
-	for ( i = str1.length - 1; i >= 0; i--)
-		 newstr += str1[i];
-	 console.log(newstr);
-}
+// function reversestr(str1) {
+// 	newstr =""
+// 	for ( i = str1.length - 1; i >= 0; i--)
+// 		 newstr += str1[i];
+// 	 console.log(newstr);
+// }
 
-reversestr("blood")
+// reversestr("blood")
 
-function capitalizefirstletter(str1) {
-	str2 = str1.split(" ");
-	text = ""
-	for (i = 0; i < str2.length; i++){
-		let tempstr = str2[i]
-		tempstr = tempstr.charAt(0).toUpperCase() + tempstr.substring(1, tempstr.length + 1);
-		text += tempstr + " "
+// function capitalizefirstletter(str1) {
+// 	str2 = str1.split(" ");
+// 	text = ""
+// 	for (i = 0; i < str2.length; i++){
+// 		let tempstr = str2[i]
+// 		tempstr = tempstr.charAt(0).toUpperCase() + tempstr.substring(1, tempstr.length + 1);
+// 		text += tempstr + " "
+// 	}
+// 	console.log(text)
+// }
+
+// capitalizefirstletter("hey how are you")
+
+// function compressstr(str3) {
+// 	temptext = ""
+// 	counter = 1
+// 	for (i = 0; i < str3.length; i++) {
+// 		if (str3.charCodeAt(i) == str3.charCodeAt(i + 1)) {
+// 			counter += 1
+// 		}
+// 		else {
+// 			temptext += counter + str3[i]
+// 			counter = 1
+// 		}
+// 	}
+// 	console.log(temptext)
+// }
+
+// compressstr("aaabbbbbccccaacccbbbaaabbbaaa")
+
+// function palindrome(palindroma) {
+// 	palindrom = palindroma.split(" ");
+// 	palindrom = palindrom.join("");
+// 	palindrom = palindrom.toUpperCase();
+// 	confirmation = ""
+// 	 for (i = 0; i < palindrom.length; i++) {
+// 		if (palindrom.charCodeAt(i) === palindrom.charCodeAt((palindrom.length-1) - i)) {
+// 			confirmation += "t" 
+// 		}
+// 		else {
+// 			confirmation += "f"
+// 		}
+// }
+// 	if (confirmation.search("f") == -1) {
+// 		console.log("This is a palindrome")
+// 	}
+// 	else {
+// 		console.log("This is not a palindrome")
+// 	}
+// console.log (confirmation)	
+// console.log (confirmation.search("f"))
+// console.log (palindrom)
+// }
+
+// palindrome("race car")
+
+// function primenum() {
+// 	for (i = 0; i <= 100; i++) {
+// 		if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0)) {
+// 			console.log("not prime")
+// 		}
+// 		else {
+// 			console.log(i)
+// 		}
+// 	}
+
+// }
+// primenum()
+
+
+
+// function happynumber(numba) {
+// 	let numba = numba.toString();
+// 	let tempnum = 0;
+// 	let othernumbers = [];
+// 	while (tempnum != 1 && true) {
+// 		for (i = 0; i < numba.length; i++) {
+// 	 		tempnum += parseInt(numba[i] * numba[i])
+// 	 	}
+// 	 	numba = tempnum;
+// 	 	tempnum = 0	
+// 	 	othernumbers.push(tempnum)
+// }
+// 	if (tempnum == 1){
+// 		console.log( "tempnum is happy")
+// 	else {
+// 		console.log( "tempnum is not happy")
+// 	}
+// 	}
+// }
+
+// happynumber(68)
+
+let magicvalue= 400;
+let strengthvalue = 300;
+let rangevalue = 350;
+let strengthdefensevalue = 100;
+let magicdefensevalue = 70;
+let rangedefensevalue =100;
+
+let daysroll = diceroll(7);
+let randomfactor = diceroll(100);
+let randommelee = diceroll(300);
+let randommagic = diceroll(200);
+let randomrange = diceroll(250);
+let randomenemymelee = diceroll(150);
+let randomenemymagic = diceroll(80);
+let randomenemyrange = diceroll(100);
+let randommelee2 = diceroll(30)
+let randommagic2 = diceroll(10)
+let randomrange2 = diceroll(20)
+
+let enemymagic = 400 + (5 * daysroll);
+let enemystrength = 300 + (5 * daysroll);
+let enemyrange = 350 + (5 * daysroll);
+let enemystrengthdefense = 100 + (5 * daysroll);
+let enemymagicdefense = 70 + (5 * daysroll);
+let enemyrangedefense = 100 + (5 * daysroll);
+let enemymeleeunits = diceroll(1000) + 1000;
+let enemymagicunits = diceroll(1000) + 1000;
+let enemyrangeunits = diceroll(1000) + 1000;
+let meleeunits = diceroll(500) + 500;
+let magicunits = diceroll(500) + 500;
+let rangeunits = diceroll(500) + 500;
+
+let armyvalue = meleeunits + magicunits + rangeunits
+let enemyarmyvalue = enemymeleeunits + enemymagicunits + enemyrangeunits
+
+
+function knights() {
+	let enemyknights = diceroll(3)
+	if (enemyknights == 1) {
+		armyvalue -= ((enemystrength * 2) - strengthdefensevalue)
+		enemyarmyvalue -= ((strengthvalue * 2) - enemystrengthdefense)
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
+	}	else if (enemyknights == 2) {
+		armyvalue -= ((enemymagic * 3) - strengthdefensevalue)
+		enemyarmyvalue -= ((strengthvalue * 2) - (enemymagicdefense * 2))
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
+	}	else  {
+		armyvalue -= ((enemyrange * 2) - strengthdefensevalue)
+		enemyarmyvalue -= ((strengthvalue * 3) - enemyrangedefense)
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}
-	console.log(text)
 }
 
-capitalizefirstletter("hey how are you")
 
-function compressstr(str3) {
-	temptext = ""
-	counter = 1
-	for (i = 0; i < str3.length; i++) {
-		if (str3.charCodeAt(i) == str3.charCodeAt(i + 1)) {
-			counter += 1
-		}
-		else {
-			temptext += counter + str3[i]
-			counter = 1
-		}
+function mages() {
+	let enemymages = diceroll(3)
+	if (enemymages == 1) {
+		armyvalue -= ((enemyknights * 2)  - (magicdefensevalue * 2))
+		enemyarmyvalue -= ((magicvalue * 3) - enemystrengthdefense)
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
+	}	else if (enemymages == 2) {
+		armyvalue -= ((enemyknights * 2) - (magicdefensevalue * 2))
+		enemyarmyvalue -= ((strengthvalue * 2) - (enemymagicdefense * 2))
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
+	}	else  {
+		armyvalue -= ((enemyrange * 3) - magicdefensevalue)
+		enemyarmyvalue -= ((magicvalue * 2) - (enemyrangedefense * 2))
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}
-	console.log(temptext)
-}
+}	
 
-compressstr("aaabbbbbccccaacccbbbaaabbbaaa")
-
-function palindrome(palindroma) {
-	palindrom = palindroma.split(" ");
-	palindrom = palindrom.join("");
-	palindrom = palindrom.toUpperCase();
-	confirmation = ""
-	 for (i = 0; i < palindrom.length; i++) {
-		if (palindrom.charCodeAt(i) === palindrom.charCodeAt((palindrom.length-1) - i)) {
-			confirmation += "t" 
-		}
-		else {
-			confirmation += "f"
-		}
-}
-	if (confirmation.search("f") == -1) {
-		console.log("This is a palindrome")
+function archers() {
+	let enemyarchers = diceroll(3)
+	if (enemyarchers == 1) {
+		armyvalue -= ((enemyknights * 3)  - rangedefensevalue )
+		enemyarmyvalue -= ((rangevalue * 2) - (enemystrengthdefense * 2))
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
+	}	else if (enemyarchers == 2) {
+		armyvalue -= ((enemymagic * 2) - (rangedefensevalue * 2))
+		enemyarmyvalue -= ((rangevalue * 3) - enemymagicdefense)
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
+	}	else  {
+		armyvalue -= ((enemyrange * 2) - rangedefensevalue)
+		enemyarmyvalue -= ((rangevalue * 2) - enemyrangedefense)
+		console.log("You have " + armyvalue + " troops.")
+		console.log("The enemy have " + enemyarmyvalue + " troops.")
 	}
-	else {
-		console.log("This is not a palindrome")
-	}
-console.log (confirmation)	
-console.log (confirmation.search("f"))
-console.log (palindrom)
+}	
+
+function diceroll(sides) {
+	sides = parseInt(sides);
+	 return Math.floor(Math.random() * (sides) + 1);
 }
 
-palindrome("race car")
-
-function primenum() {
-	for (i = 0; i <= 100; i++) {
-		if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0)) {
-			console.log("not prime")
-		}
-		else {
-			console.log(i)
-		}
-	}
-
+function turn() {
+let whichunit = prompt("Choose either knights, mages, archers, or Stop.")
+if (whichunit == "knights"){ 
+knights();
+}	else if ( whichunit =="mages") {
+	mages();
+}	else if (whichunit =="archers") {
+	archers();
+}	else if (whichunit == "stop"){
+		
+}	else {
+	alert("Please enter knights, mages, archers, or stop!");
+	turn();
 }
-primenum()
-
-
-
-function happynumber(numba) {
-	let numba = numba.toString();
-	let tempnum = 0;
-	let othernumbers = [];
-	while (tempnum != 1 && true) {
-		for (i = 0; i < numba.length; i++) {
-	 		tempnum += parseInt(numba[i] * numba[i])
-	 	}
-	 	numba = tempnum;
-	 	tempnum = 0	
-	 	othernumbers.push(tempnum)
-}
-	if (tempnum == 1){
-		console.log( "tempnum is happy")
-	else {
-		console.log( "tempnum is not happy")
-	}
-	}
 }
 
-happynumber(68)
+
+while (armyvalue >= 0 && enemyarmyvalue >= 0) {
+	turn()
+}
