c5d22bf6d737d4169716647b3c6c474fee707a28
henry huynh <hlhuynh14@gmail.com>
2019-04-23 08:06:31 -0500

    add several of new dialogues show stats of current enemy army as well as your current stats

--- DiceGame.js
+++ DiceGame.js
@@ -3,25 +3,28 @@ function dice(sides) {
 	 return Math.floor(Math.random() * (sides) + 1)
 }
 
-console.log("War is coming to your city, you must prepare your limited troops and supplies, then face them out front!")
+console.log("War is coming to your city, you must prepare your limited troops and supplies, then face the enemy out front!")
 
-magic= 100
-strength = 100
-range = 100
-strengthdefense = 100
-magicdefense = 100
-rangedefense =100
+let magicvalue= 100
+let strengthvalue = 100
+let rangevalue = 100
+let strengthdefensevalue = 100
+let magicdefensevalue = 100
+let rangedefensevalue =100
+
+let daysroll = dice(7)
+
+let enemymagic = 60 + (5 * daysroll)
+let enemystrength = 60 + (5 * daysroll)
+let enemyrange = 60 + (5 * daysroll)
+let enemystrengthdefense = 60 + (5 * daysroll)
+let enemymagicdefense = 60 + (5 * daysroll)
+let enemyrangedefense = 60 + (5 * daysroll)
+let enemymeeleeunits = dice(1000) + 1000
+let enemymagicunits = dice(1000) + 1000
+let enemyrangeunits = dice(1000) + 1000
 
 
-enemymagic = 80
-enemystrength = 80
-enemyrange = 80
-enemystrengthdefense = 80
-enemymagicdefense = 80
-enemyrangedefense = 80
-
-daysroll = dice(7)
-console.log(daysroll)
 if ( daysroll == 1) {
 	console.log("Ooh, too bad, you only have one day to prepare, hope you are lucky for the battle!")
 }	else if (daysroll == 2) {
@@ -38,12 +41,20 @@ if ( daysroll == 1) {
 		console.log("Good job, you have a week to prepare for the siege, this will be an easy victory!")
 }
 
-console.log(" Your melee troops current strength is" + " " + strength)
-console.log(" Your melee troops current defense is" + " " + strengthdefense)
-console.log(" Your magic troops current magic is" + " " + magic)
-console.log(" Your magic troops current defense is" + " " + magicdefense)
-console.log(" Your range troops current range is" + " " + range)
-console.log(" Your range troops current defense is" + " " +rangedefense)
+console.log("Your scout has determined the invading forces.")
+console.log(enemymeeleeunits + " meelee units will be in the the enemy army.")
+console.log(enemymagicunits + " magic units will be in the the enemy army.")
+console.log(enemyrangeunits + " range units will be in the the enemy army.")
+
+console.log(" Your melee troops current strength is" + " " + strengthvalue)
+console.log(" Your melee troops current defense is" + " " + strengthdefensevalue)
+console.log(" Your magic troops current magic is" + " " + magicvalue)
+console.log(" Your magic troops current defense is" + " " + magicdefensevalue)
+console.log(" Your range troops current range is" + " " + rangevalue)
+console.log(" Your range troops current defense is" + " " +rangedefensevalue)
+
+
+
 
 
 
